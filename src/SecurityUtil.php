db23cf1e90f9112f5ecfd64976bed16d53dc04fd
volkangunaydin <96166649+volkangunaydin@users.noreply.github.com>
2025-07-16 14:14:12 +0300

    -

--- src/SecurityUtil.php
+++ src/SecurityUtil.php
@@ -8,7 +8,7 @@ class SecurityUtil{
     }
     public static function getGUID(){
             if (function_exists('com_create_guid') === true){
-                return  trim(com_create_guid(), '{}');
+                return  trim(com_create_guid(), '{}'); 
             }else{
                  return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
             }
