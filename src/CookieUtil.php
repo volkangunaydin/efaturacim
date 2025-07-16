3e8ca64ddf925f7bc6334f410e9ced0283a1611b
volkangunaydin <96166649+volkangunaydin@users.noreply.github.com>
2025-07-16 12:18:31 +0300

    -

---
+++ src/CookieUtil.php
@@ -0,0 +1,34 @@
+<?php
+namespace Efaturacim\Util;
+class CookieUtil{
+    public static function setCookie($name,$value,$expInDays=null){
+        if(is_null($expInDays) || $expInDays<=0){
+            $t = time() + (86400 * 30*365);
+        }else{
+            $t = time() + round((86400 * $expInDays));
+        }
+        setcookie($name,$value,expires_or_options: $t);
+    }
+    public static function getCookie($name,$defValOrCallback=null,$expInDays=null){
+        if(!is_null($name) && strlen("".$name)>0){
+            if(key_exists($name, $_COOKIE)){
+                return @$_COOKIE[$name];
+            }else if (!is_null($defValOrCallback)){
+                if(is_callable($defValOrCallback)){
+                    $val = call_user_func_array($defValOrCallback,array());
+                    if(!is_null($val)){
+                        self::setCookie($name,$val,$expInDays);
+                    }                                        
+                    return $val;
+                }else if (is_scalar($defValOrCallback)){
+                    return $defValOrCallback;
+                }else{
+                    return null;
+                }
+            }else{
+                return null;
+            }
+        }            
+    }
+}
+?>
---
+++ src/SecurityUtil.php
@@ -0,0 +1,17 @@
+<?php
+namespace Efaturacim\Util;
+class SecurityUtil{
+    public static function getClientKey(){
+        return CookieUtil::getCookie("SECURE_CLIENT_KEY",function(){
+            return SecurityUtil::getGUID();
+        });   
+    }
+    public static function getGUID(){
+            if (function_exists('com_create_guid') === true){
+                return  trim(com_create_guid(), '{}');
+            }else{
+                 return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+            }
+    }
+}
+?>
