d97f4af750c6b1693e57dad5377bc2bde1c8100f
volkangunaydin <96166649+volkangunaydin@users.noreply.github.com>
2025-08-15 08:54:21 +0300

    -

---
+++ src/Utils/Usage/Html/DataTableUsage.php
@@ -0,0 +1,34 @@
+<?php
+namespace Efaturacim\Util\Usage\Html;
+
+use Efaturacim\Util\Data\GeoData\TR\Iller;
+use Efaturacim\Util\Utils\Html\Bootstrap\Row;
+use Efaturacim\Util\Utils\Html\HtmlDocument;
+use Efaturacim\Util\Utils\Html\Datatable\DataTablesJs;
+use Efaturacim\Util\Utils\Html\PrettyPrint\PrettyPrint;
+
+class DataTableUsage{
+    public static function getDemoHtml(&$doc){
+        $s = '';
+        $s .= self::getDemoHtmlForStaticDataTable($doc);
+        return $s;
+    }
+    public static function getDemoHtmlForStaticDataTable(&$doc){
+        $s = '';
+    
+        // STATIC DATA TABLE
+        $s .= '<h3>SABİT VERİLER İLE DATATABE OLUŞTURMA</h3>';
+        $dataTableStatic = DataTablesJs::newTable("#","İl Adı","Plaka","Enlem","Boylam","HTML");        
+        $dataTableStatic->addStaticData(Iller::getIller(),function($index,$row){
+            $htmlStr = '-';
+            return [$index,@$row["adi"],@$row["plaka"],@$row["lat"],@$row["long"],$htmlStr];
+        });
+        $s .= ($dataTableHtml = $dataTableStatic->toHtml($doc));
+        $strHtml = ''.PrettyPrint::html($doc,$dataTableHtml,null,400);
+        $strJs   = ''.PrettyPrint::js($doc,$dataTableStatic->getJsLinesForDebug(),null,400);
+        $s .= "".Row::newRow()->col6($strHtml)->col6($strJs);
+        // END OF STATIC DATA TABLE
+        return $s;
+    }
+}
+?>
