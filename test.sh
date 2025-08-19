c0c76dffe64485a53ba5dc0f4870067a334fd055
IlimDenizTpr <ilimdenizty@gmail.com>
2025-08-19 22:49:14 +0300

    Servis teste firma bilgisi güncelleme yazıldı

--- test.sh
+++ test.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Change directory to the location of this script to ensure paths are correct.
 cd "$(dirname "$0")"
-#Macbookda çalışması için yapıldı .bat dosyası çalışmıyor macbookda windows için ayarlanmış
-./vendor/bin/phpunit
+
+# Check if a parameter was passed
+if [ "$1" = "ServisTest" ]; then
+    echo "Running ServisTest only..."
+    ./vendor/bin/phpunit tests/EfaturacimServisTest/ServisTest.php
+else
+    echo "Running all tests..."
+    ./vendor/bin/phpunit
+fi
+
+# Pause the console to see the output before it closes.
+read -p "Press any key to continue..."
