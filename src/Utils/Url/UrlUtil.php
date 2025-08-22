77da99faa6502037b491b84959974eca45267016
volkangunaydin <96166649+volkangunaydin@users.noreply.github.com>
2025-08-22 10:17:35 +0300

    Version 1.0.112

--- src/Utils/Url/UrlUtil.php
+++ src/Utils/Url/UrlUtil.php
@@ -13,24 +13,24 @@ class UrlUtil{
             return $_SERVER['HTTP_X_FORWARDED_HOST'];
         } elseif (isset($_SERVER['HTTP_HOST'])) {
             // Standard host check (for non-proxied setups).
-            return  $_SERVER['HTTP_HOST'];
+            return  @$_SERVER['HTTP_HOST'];
         } else {
             // Fallback if HTTP_HOST isn't set.
-            return $_SERVER['SERVER_NAME'];
+            return @$_SERVER['SERVER_NAME'];
         }    
     }
     public static function getBaseUrl(){        
         $isSsl = self::isSsl();
         if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
             // The proxy has provided the original host requested by the client.
-            $host =  $_SERVER['HTTP_X_FORWARDED_HOST'];
+            $host =  @$_SERVER['HTTP_X_FORWARDED_HOST'];
         } elseif (isset($_SERVER['HTTP_HOST'])) {
             // Standard host check (for non-proxied setups).
-            $host = $_SERVER['HTTP_HOST'];
+            $host = @$_SERVER['HTTP_HOST'];
         } else {
             // Fallback if HTTP_HOST isn't set.
-            $host = $_SERVER['SERVER_NAME'];
-            $port     = $_SERVER['SERVER_PORT'];
+            $host     = @$_SERVER['SERVER_NAME'];
+            $port     = @$_SERVER['SERVER_PORT'];
             $isStandardPort = (!$isSsl && $port == 80) || ($isSsl && $port == 443);
             if (!$isStandardPort) {
                 $host .= ':' . $port;
