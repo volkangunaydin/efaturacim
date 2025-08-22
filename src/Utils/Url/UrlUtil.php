5ae2097b11a77e3533ecdcb763614218403f95d7
volkangunaydin <96166649+volkangunaydin@users.noreply.github.com>
2025-08-22 10:26:08 +0300

    Version 1.0.113

--- src/Utils/Url/UrlUtil.php
+++ src/Utils/Url/UrlUtil.php
@@ -1,41 +1,43 @@
 <?php
 namespace Efaturacim\Util\Utils\Url;
 class UrlUtil{
+    protected static $__isSSL = null;
+    protected static $__baseHost = null;
     public static function getUrl($url=null,$newParams=null,$excludeParams=null){
         return new UrlObject($url,$newParams,$excludeParams);
     }
-    public static function isSsl(){        
-        return ((isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') ||  isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] === 'on' || $_SERVER['HTTPS'] == 1));                
+    public static function isSsl(){    
+        if(is_null(self::$__isSSL)){
+            self::$__isSSL = ((isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') ||  isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] === 'on' || $_SERVER['HTTPS'] == 1));
+        }
+        return self::$__isSSL;
     }
     public static function getBaseHost(){
-        if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-            // The proxy has provided the original host requested by the client.
-            return $_SERVER['HTTP_X_FORWARDED_HOST'];
-        } elseif (isset($_SERVER['HTTP_HOST'])) {
-            // Standard host check (for non-proxied setups).
-            return  @$_SERVER['HTTP_HOST'];
-        } else {
-            // Fallback if HTTP_HOST isn't set.
-            return @$_SERVER['SERVER_NAME'];
-        }    
+        if(is_null(self::$__baseHost)){
+            $isSsl = self::isSsl();
+            if (!isset($_SERVER)) {
+                self::$__baseHost = "";
+            }else if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+                // The proxy has provided the original host requested by the client.
+                self::$__baseHost =  $_SERVER['HTTP_X_FORWARDED_HOST'];
+            } elseif (isset($_SERVER['HTTP_HOST'])) {
+                // Standard host check (for non-proxied setups).
+                self::$__baseHost =   @$_SERVER['HTTP_HOST'];
+            } else {
+                // Fallback if HTTP_HOST isn't set.
+                self::$__baseHost =  @$_SERVER['SERVER_NAME'];                
+                $port     = @$_SERVER['SERVER_PORT'];
+                $isStandardPort = (!$isSsl && $port == 80) || ($isSsl && $port == 443);
+                if (!$isStandardPort) {
+                    self::$__baseHost .= ':' . $port;
+                }                            
+            }    
+        }            
+        return self::$__baseHost;
     }
     public static function getBaseUrl(){        
         $isSsl = self::isSsl();
-        if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-            // The proxy has provided the original host requested by the client.
-            $host =  @$_SERVER['HTTP_X_FORWARDED_HOST'];
-        } elseif (isset($_SERVER['HTTP_HOST'])) {
-            // Standard host check (for non-proxied setups).
-            $host = @$_SERVER['HTTP_HOST'];
-        } else {
-            // Fallback if HTTP_HOST isn't set.
-            $host     = @$_SERVER['SERVER_NAME'];
-            $port     = @$_SERVER['SERVER_PORT'];
-            $isStandardPort = (!$isSsl && $port == 80) || ($isSsl && $port == 443);
-            if (!$isStandardPort) {
-                $host .= ':' . $port;
-            }            
-        }           
+        $host  = self::getBaseHost();        
         return $isSsl ? 'https://' : 'http://' . $host."/";
     }
 }
