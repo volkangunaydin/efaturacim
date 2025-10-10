18df6b65b6ef871a7d2aa6d0e9d620590d49a64d
ozgurorkestra <167961920+ozgurorkestra@users.noreply.github.com>
2025-10-10 10:02:59 +0300

    -

--- src/Ubl/Preview/UblPreview.php
+++ src/Ubl/Preview/UblPreview.php
@@ -5,6 +5,7 @@ class UblPreview{
     public static function getHtmlFromXml($xmlString,$xsltString,$options=null){
         return XsltUtil::showXmlFileWithXslt($xmlString,$xsltString,$options);
     }
+    
 }
 
 ?>
